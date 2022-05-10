69c00ed757759a34facbe2659caff4863269b7c2
Derrick <derrickkueh1995@gmail.com>
2022-05-10 12:59:43 +0800

    IR10 - Fix recalibration and reorganise code structure
    1. variables are moved to the top
    2. functions and windows to the bottom
    3. lines that require regular calibration and one time set is separated

--- script/main.js
+++ script/main.js
@@ -114,36 +114,52 @@ let current_title = 99;
 let inStats = 0;
 let inSkills = 0;
 
-let testtest = 0;
-let testtest2 = 0;
-let testtest3 = 0;
-let testtest4 = 0;
-let testtest5 = 0;
-let testtest6 = 0;
-let testtest7 = 0;
-let testtest8 = 0;
+let portrait1Left = 0;
+let portrait1Right = 0;
+let portrait2Left = 0;
+let portrait2Right = 0;
+let portrait3Left = 0;
+let portrait3Right = 0;
+let portrait4Left = 0;
+let portrait4Right = 0;
 
 let realcontent = $("#realcontent");
 let fakecontent = $("#fakecontent");
 
-//-----------------------------------------------------------------------------------------------------------------------------------
-// set pointer event to none means we can't click the object
-$(player).css("pointer-events", "none");
-stats.css("pointer-events", "none");
-skills.css("pointer-events", "none");
-$("#stat-wrapper").css("pointer-events", "none");
-
-//-----------------------------------------------------------------------------------------------------------------------------------
 // query screen size
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 /// potential better alternative: var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-// set up fake vertical scrolling height
-realcontent.css("height", vh);
-let realwidth = realcontent.css("width");
-let fakeheight = parseInt(realwidth, 10) + vh-vw;
-fakecontent.css("height", fakeheight+"px");
+let lastScrollTop = pageYOffset || 0;
+
+let groundHeight = 0;
+
+let currentposition = 0;
+
+let statpositionstart = 0;
+let statpositionend = 0;
+let skillpositionstart = 0;
+let skillpositionend = 0;
+let scene1end = 0;
+let scene2end = 0;
+let scene3end = 0;
+let scene4end = 0;
+let scene5end = 0;
+let scene6end = 0;
+
+// Revealing the buildings' interior
+let uniLeft = 0;
+let revealUniLeft = 0;
+let revealUniRight = 0;
+
+let intelLeft = 0;
+let revealIntelLeft = 0;
+let revealIntelRight = 0;
+
+let houseLeft = 0;
+let revealHouseLeft = 0;
+let revealHouseRight = 0;
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 // set background
@@ -161,26 +177,6 @@ ground3.css("background-image", 'url(image/export/ground-squarestone.png)');
 ground4.css("background-image", 'url(image/export/ground-squarestone.png)');
 ground5.css("background-image", 'url(image/export/ground-magical.png)');
 ground6.css("background-image", 'url(image/export/ground-tot.png)');
-
-// Set groundheight for players and other objects to stand on ground
-let groundHeight = ground1.css("height");
-player.css("bottom", groundHeight);
-
-//-----------------------------------------------------------------------------------------------------------------------------------
-// set sprites css.position -> absolute
-for (i = 0; i < sprites_array.length; i++) {
-  sprites_array[i].attr('draggable', false);
-  sprites_array[i].css("position", "absolute");
-  sprites_array[i].css("bottom", groundHeight);
-}
-facebook_icon.css("bottom", "74.3%");
-instagram_icon.css("bottom", "74.3%");
-linkedin_icon.css("bottom", "74.3%");
-itchio_icon.css("bottom", "74.3%");
-github_icon.css("bottom", "74.3%");
-
-email_icon.css("bottom", "40%");
-resume.css("bottom", "40%");
 
 $(facebook_icon).css("z-index", "500");
 $(facebook_icon).wrap($('<a>',{
@@ -225,152 +221,36 @@ $(resume).wrap($('<a>',{
 }).attr('target','_blank'));
 
 //-----------------------------------------------------------------------------------------------------------------------------------
-// Set the position and height of the sprites based on scenes
+// set pointer event to none means we can't click the object
+$(player).css("pointer-events", "none");
+stats.css("pointer-events", "none");
+skills.css("pointer-events", "none");
+$("#stat-wrapper").css("pointer-events", "none");
 
-lowerball.css("height", "20%");
-upperball.css("height", "20%");
+//-----------------------------------------------------------------------------------------------------------------------------------
+// Set the position and height of the sprites based on vw and vh
 
-// Scene 1 - Self Intro
-let currentposition = 0;
-setrelativevwh(pinktree, 5, 70);
-setrelativevwh(signboard, 5, 20);
-let statpositionstart = currentposition;
-let statpositionend = currentposition + vw;
-currentposition += vw;
-let skillpositionstart = currentposition + vw/2;
-currentposition += vw/2;
-let skillpositionend = currentposition + vw;
-currentposition += vw * 2;
-// Scene 2 - Studies 
-let scene1end = currentposition - vw/2;
-setrelativevwh(ukflag, 0, 70);
-setrelativevwh(universitystone, 30, 15);
-setrelativevwh(university_inside, -10, 70, 0);
-setrelativevwh(university_outside, -10, 70);
-// Scene 3 - Work Experience
-let scene2end = currentposition;
-setrelativevwh(malaysianflag, 50, 70);
-setrelativevwh(intel_inside, 30, 70, 0);
-setrelativevwh(intel_outside, 30, 70);
-// Scene 4 - Projects
-let scene3end = currentposition;
-setrelativevwh(homesign, 50, 30);
-setrelativevwh(house_inside, 10, 50, 0);
-setrelativevwh(house_outside, 10, 50);
-// Scene 5 - Interests
-let scene4end = currentposition;
-setrelativevwh(chess, 100, 20);
-setrelativevwh(wip3, 20, 20);
-// Scene 6 - Contact Me
-let scene5end = currentposition;
-setrelativevwh(door, 120, 71, 0, 1);
-setrelativevwh(facebook_icon, 120, 10.1, 0, 1);
-setrelativevwh(pillar, 120, 85, 1, 1);
-setrelativevwh(door2, 0, 71, 0, 1);
-setrelativevwh(instagram_icon, 0, 10.1, 0, 1);
-setrelativevwh(pillar2, 0, 85, 1, 1);
-setrelativevwh(door3, 0, 71, 0, 1);
-setrelativevwh(linkedin_icon, 0, 10.1, 0, 1);
-setrelativevwh(pillar3, 0, 85, 1, 1);
-setrelativevwh(door4, 0, 71, 0, 1);
-setrelativevwh(itchio_icon, 0, 10.1, 0, 1);
-setrelativevwh(pillar4, 0, 85, 1, 1);
-setrelativevwh(door5, 0, 71, 0, 1);
-setrelativevwh(github_icon, 0, 10.1, 0, 1);
-setrelativevwh(pillar5, 0, 85, 1, 1);
-setrelativevwh(email_icon, 0, 8, 0, 1);
-setrelativevwh(resume_holder, 0, 20, 1, 1);
-setrelativevwh(resume, 1, 8, 0, 1);
-setrelativevwh(resume_holder2, 1, 20, 1, 1);
-// Scene 7 - Chair to sit on
-setrelativevwh(relaxer, 30, 10);
-let scene6end = currentposition;
-currentposition+=vw/2;
-realcontent.css("width", currentposition + "px");
-fakecontent.css("height", currentposition + vh - vw + "px");
+recalibrate();
 
-// set ground width
-ground1.css("width", currentposition + vw + "px");
-ground2.css("width", currentposition + vw + "px");
-ground3.css("width", currentposition + vw + "px");
-ground4.css("width", currentposition + vw + "px");
-ground5.css("width", currentposition + vw + "px");
-ground6.css("width", currentposition + vw + "px");
 
-// preload settings
-$(window).on('load', function(){
-
-  testtest = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.365;
-  testtest2 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.395;
-
-  testtest3 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.521;
-  testtest4 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.551;
-
-  testtest5 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.676;
-  testtest6 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.706;
-
-  testtest7 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.831;
-  testtest8 = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.861;
-
-  $("#preloader").css("opacity", 0);
-  setTimeout(function(){ $("#preloader").hide(); }, 2000);
-});
-
-// Revealing the buildings' interior
-let uniLeft = university_outside.css("left");
-let revealUniLeft = parseInt(uniLeft, 10) - vw/2;
-let revealUniRight = revealUniLeft + university_outside.width();
-
-let intelLeft = intel_outside.css("left");
-let revealIntelLeft = parseInt(intelLeft, 10) - vw/2;
-let revealIntelRight = revealIntelLeft + intel_outside.width();
-
-let houseLeft = house_outside.css("left");
-let revealHouseLeft = parseInt(houseLeft, 10) - vw/2;
-let revealHouseRight = revealHouseLeft + house_outside.width();
-
+// change stats and skills image to smaller for mobile screens
 if (window.matchMedia('(max-width: 768px)').matches)
 {
   stats.attr("src","image/export/sprites/statsmobile.png");
   skills.attr("src","image/export/sprites/skillsmobile.png");
 }
 
-
 // Resize window will recalibrate all parameters
 window.addEventListener("resize", recalibrate);
 
-function recalibrate() {
-  // set height of background;
-  vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-  /// potential better alternative: var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+// preload settings
+$(window).on('load', function(){
 
-  realcontent.css("height", vh);
-  realwidth = realcontent.css("width");
-  fakeheight = parseInt(realwidth, 10) + vh-vw;
-  fakecontent.css("height", fakeheight+"px");
-  ground1.css("width", realwidth);
-  groundHeight = ground1.css("height");
-
-  player.css("bottom", groundHeight);
-  for (i = 0; i < sprites_array.length; i++) {
-    sprites_array[i].css("position", "absolute");
-    sprites_array[i].css("bottom", groundHeight);
-  }
-
-  facebook_icon.css("bottom", "74.3%");
-  instagram_icon.css("bottom", "74.3%");
-  linkedin_icon.css("bottom", "74.3%");
-  itchio_icon.css("bottom", "74.3%");
-  github_icon.css("bottom", "74.3%");
-
-  email_icon.css("bottom", "50%");
-  resume.css("bottom", "50%");
-}
-
+  $("#preloader").css("opacity", 0);
+  setTimeout(function(){ $("#preloader").hide(); }, 2000);
+});
 
 // Main scroll event
-let lastScrollTop = pageYOffset || 0;
 $(window).on('scroll', function() {
   // Change background and ground
   changeScene();
@@ -481,6 +361,131 @@ $(window).on('scroll', function() {
 //   console.log("pageyoffset is " + window.pageYOffset);
 });
 
+function recalibrate() {
+  // set height of background;
+  vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  /// potential better alternative: var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+ 
+  // set real content div height
+  realcontent.css("height", vh);
+
+  // Scene 1 - Self Intro
+  currentposition = 0;
+  setrelativevwh(pinktree, 5, 70);
+  setrelativevwh(signboard, 5, 20);
+  statpositionstart = currentposition;
+  statpositionend = currentposition + vw;
+  currentposition += vw;
+  skillpositionstart = currentposition + vw/2;
+  currentposition += vw/2;
+  skillpositionend = currentposition + vw;
+  currentposition += vw * 2;
+  // Scene 2 - Studies 
+  scene1end = currentposition - vw/2;
+  setrelativevwh(ukflag, 0, 70);
+  setrelativevwh(universitystone, 30, 15);
+  setrelativevwh(university_inside, -10, 70, 0);
+  setrelativevwh(university_outside, -10, 70);
+  // Scene 3 - Work Experience
+  scene2end = currentposition;
+  setrelativevwh(malaysianflag, 50, 70);
+  setrelativevwh(intel_inside, 30, 70, 0);
+  setrelativevwh(intel_outside, 30, 70);
+  // Scene 4 - Projects
+  scene3end = currentposition;
+  setrelativevwh(homesign, 50, 30);
+  setrelativevwh(house_inside, 10, 50, 0);
+  setrelativevwh(house_outside, 10, 50);
+  // Scene 5 - Interests
+  scene4end = currentposition;
+  setrelativevwh(chess, 100, 20);
+  setrelativevwh(wip3, 20, 20);
+  // Scene 6 - Contact Me
+  scene5end = currentposition;
+  setrelativevwh(door, 120, 71, 0, 1);
+  setrelativevwh(facebook_icon, 120, 10.1, 0, 1);
+  setrelativevwh(pillar, 120, 85, 1, 1);
+  setrelativevwh(door2, 0, 71, 0, 1);
+  setrelativevwh(instagram_icon, 0, 10.1, 0, 1);
+  setrelativevwh(pillar2, 0, 85, 1, 1);
+  setrelativevwh(door3, 0, 71, 0, 1);
+  setrelativevwh(linkedin_icon, 0, 10.1, 0, 1);
+  setrelativevwh(pillar3, 0, 85, 1, 1);
+  setrelativevwh(door4, 0, 71, 0, 1);
+  setrelativevwh(itchio_icon, 0, 10.1, 0, 1);
+  setrelativevwh(pillar4, 0, 85, 1, 1);
+  setrelativevwh(door5, 0, 71, 0, 1);
+  setrelativevwh(github_icon, 0, 10.1, 0, 1);
+  setrelativevwh(pillar5, 0, 85, 1, 1);
+  setrelativevwh(email_icon, 0, 8, 0, 1);
+  setrelativevwh(resume_holder, 0, 20, 1, 1);
+  setrelativevwh(resume, 1, 8, 0, 1);
+  setrelativevwh(resume_holder2, 1, 20, 1, 1);
+  // Scene 7 - Chair to sit on
+  setrelativevwh(relaxer, 30, 10);
+  scene6end = currentposition;
+  currentposition+=vw/2;
+  realcontent.css("width", currentposition + "px");
+  fakecontent.css("height", currentposition + vh - vw + "px");
+
+  // set ground width
+  ground1.css("width", currentposition + vw + "px");
+  ground2.css("width", currentposition + vw + "px");
+  ground3.css("width", currentposition + vw + "px");
+  ground4.css("width", currentposition + vw + "px");
+  ground5.css("width", currentposition + vw + "px");
+  ground6.css("width", currentposition + vw + "px");
+
+  // Set groundheight for players and other objects to stand on ground
+  groundHeight = ground1.css("height");
+  player.css("bottom", groundHeight);
+
+  //-----------------------------------------------------------------------------------------------------------------------------------
+  // set sprites css.position -> absolute
+  for (i = 0; i < sprites_array.length; i++) {
+    sprites_array[i].attr('draggable', false);
+    sprites_array[i].css("position", "absolute");
+    sprites_array[i].css("bottom", groundHeight);
+  }
+
+  facebook_icon.css("bottom", "74.3%");
+  instagram_icon.css("bottom", "74.3%");
+  linkedin_icon.css("bottom", "74.3%");
+  itchio_icon.css("bottom", "74.3%");
+  github_icon.css("bottom", "74.3%");
+
+  email_icon.css("bottom", "40%");
+  resume.css("bottom", "40%");
+  lowerball.css("height", "20%");
+  upperball.css("height", "20%");
+
+  // Revealing the buildings' interior
+  uniLeft = university_outside.css("left");
+  revealUniLeft = parseInt(uniLeft, 10) - vw/2;
+  revealUniRight = revealUniLeft + university_outside.width();
+
+  intelLeft = intel_outside.css("left");
+  revealIntelLeft = parseInt(intelLeft, 10) - vw/2;
+  revealIntelRight = revealIntelLeft + intel_outside.width();
+
+  houseLeft = house_outside.css("left");
+  revealHouseLeft = parseInt(houseLeft, 10) - vw/2;
+  revealHouseRight = revealHouseLeft + house_outside.width();
+
+  portrait1Left = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.365;
+  portrait1Right = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.395;
+
+  portrait2Left = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.521;
+  portrait2Right = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.551;
+
+  portrait3Left = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.676;
+  portrait3Right = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.706;
+
+  portrait4Left = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.831;
+  portrait4Right = parseInt(revealHouseLeft,10) + parseInt(house_inside.css("width"),10)* 0.861;
+}
+
 function enterUniversity() {
   let scrollPos = $(window).scrollTop();
   if (scrollPos > revealUniLeft && scrollPos < revealUniRight) {
@@ -548,28 +553,6 @@ function activeBackground(background) {
   }
 }
 
-
-// to do:
-//// must have
-// preload the rest in SS  - nid to preload!!!!!
-// make it work on safari
-// make it mobile responsive
-
-//should have mobile frenly
-// should have cleaner code
-
-/// to fix
-//github pillar
-// resume and email when resize drop to floor
-
-//// could have
-// remove mountains and norway
-// fix player size
-// fix ground not connecting
-//contact clean up
-// easter egg
-
-
 function enterStat() {
   let scrollPos = $(window).scrollTop();
   if (scrollPos > statpositionstart && scrollPos < statpositionend) {
@@ -634,7 +617,7 @@ function enterSkill() {
 function enterPortfolio() {
   let scrollPos = $(window).scrollTop();
 
-  if (scrollPos > testtest && scrollPos < testtest2) {
+  if (scrollPos > portrait1Left && scrollPos < portrait1Right) {
 
     portfolio_img.attr("src","image/ss/interactive-resume.png");
     portfolio_header.html("Derrick's Interactive Resume");
@@ -643,7 +626,7 @@ function enterPortfolio() {
     It got me thinking'If I can animate something in Unity, what is stopping me from making something cool in my own website?' Thus, it begins. ");
     portfolio_link.attr("href","#portfolio-link");
     portfolio.fadeIn(300);
-  } else if (scrollPos > testtest3 && scrollPos < testtest4) {
+  } else if (scrollPos > portrait2Left && scrollPos < portrait2Right) {
     portfolio_img.attr("src","image/ss/sticky-slime-situation.png");
     portfolio_header.html("Sticky Slime Situation");
     portfolio_paragraph.html("This was my game submission for my second gamejam - GMTK2020. The theme was 'Out Of Control' and there was a two day time limit. My initial \
@@ -652,7 +635,7 @@ function enterPortfolio() {
     so it became a sudden death mode for the player. Way to go huh.");
     portfolio_link.attr("href","https://ahpek.itch.io/sticky-slime-situation");
     portfolio.fadeIn(300);
-  } else if (scrollPos > testtest5 && scrollPos < testtest6) {
+  } else if (scrollPos > portrait3Left && scrollPos < portrait3Right) {
     portfolio_img.attr("src","image/ss/baodesignstudio.png");
     portfolio_header.html("Bao Design Studio");
     portfolio_paragraph.html("My friend had just started his own interior design company and he told me a website would help to boost his online presence. I took it as \
@@ -663,7 +646,7 @@ function enterPortfolio() {
     builder, it was much more powerful than that. I ended up recreating the website in wordpress using Elementor and that was that. For now.");
     portfolio_link.attr("href","https://baodesignstudio.com/");
     portfolio.fadeIn(300);
-  } else if (scrollPos > testtest7 && scrollPos < testtest8) {
+  } else if (scrollPos > portrait4Left && scrollPos < portrait4Right) {
     portfolio_img.attr("src","image/ss/liar.png");
     portfolio_header.html("Loner In A Relationship");
     portfolio_paragraph.html("Community Game Jam 2019 - my first ever gamejam hosted by fellow game dev youtubers. The theme was 'liar' with a one week time limit and \
@@ -677,7 +660,6 @@ function enterPortfolio() {
     portfolio.fadeOut(300);
   }
 }
-
 
 function enterScroll() {
   let scrollPos = $(window).scrollTop();
@@ -711,3 +693,23 @@ function titleSpawn(name, titleid) {
   current_title = titleid;
   }
 }
+
+// to do:
+//// must have
+// preload the rest in SS  - nid to preload!!!!!
+// make it work on safari
+// make it mobile responsive
+
+//should have mobile frenly
+// should have cleaner code
+
+/// to fix
+//github pillar
+// resume and email when resize drop to floor
+
+//// could have
+// remove mountains and norway
+// fix player size
+// fix ground not connecting
+//contact clean up
+// easter egg
